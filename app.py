c66b4bda2be4836f2c7f89d778415cca1d90f65f
sitasharma94 <sowmyaemani5@gmail.com>
2026-03-18 19:45:23 +0530

    done with basic functionality of uploading and downloading of filtered values

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["file"]
+
+    df = pd.read_excel(file)
+
+    # Filter marks < 12
+    filtered = df[df["Marks"] < 12]
+
+    output_file = "students_below_12.xlsx"
+    filtered.to_excel(output_file, index=False)
+
+    return send_file(output_file, as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
